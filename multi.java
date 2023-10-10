29fcddbac33f917d3d505c01e409d21fe0a4c4ec
shubham_407 <khapekarshubham407@gmail.com>
2023-10-10 18:55:31 +0530

    dsa practice

---
+++ multi.java
@@ -0,0 +1,13 @@
+public class multi {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 1;
+
+        while(b<=10){
+            int c = b*a;
+            System.out.println(c);
+            b++;
+        }
+    }
+    
+}
