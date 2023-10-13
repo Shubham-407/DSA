06eb7ecdcfee8e91e2bd20792a30aa5904313703
shubham_407 <khapekarshubham407@gmail.com>
2023-10-13 17:13:06 +0530

    first commit

---
+++ Alpha.java
@@ -0,0 +1,13 @@
+public class Alpha{
+    public static void main(String[] args) {
+        for(int i = 65; i<91; i++){
+            System.out.print((char)i+" ");
+            
+        }
+        System.out.println(".......................................................................");
+        for(int i = 97; i<123; i++){
+            System.out.print((char)i+" ");
+            
+        }
+    }
+}
