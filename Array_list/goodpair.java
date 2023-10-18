f3a4957e39a7d4e971637130f2034729b694353e
shubham_407 <khapekarshubham407@gmail.com>
2023-10-18 22:01:09 +0530

    first commit

---
+++ Array_list/goodpair.java
@@ -0,0 +1,20 @@
+package Array_list;
+
+public class goodpair {
+    static int pair(int [] nums){
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            for(int j =i+1;j<nums.length;j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,1,1,3};
+        System.out.println("Number of count " + pair(nums));
+    }
+    
+}
